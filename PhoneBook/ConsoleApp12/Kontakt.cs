7adf9aa4a0b89f9cc65d1d1bc69aeda34c866e09
Marcel Klukiewicz <109680195+Mklukiewicz99@users.noreply.github.com>
2022-07-20 17:13:01 +0200

    1 Project

---
+++ PhoneBook/ConsoleApp12/Kontakt.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime;
+
+namespace ConsoleApp12
+{
+    class Kontakt
+    {
+        public String Name;
+        public String Nr;
+
+        public Kontakt(string name, string nr)
+        {
+            Name = name;
+            Nr = nr;
+            Console.WriteLine("Utworzono Kontakt");
+        }
+                
+          
+        
+    }
+}
---
+++ PhoneBook/ConsoleApp12/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+
+class Zadanie 
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Podaj x");
+        int X = int.Parse(Console.ReadLine());
+        Console.WriteLine("Podaj y");
+        int y = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("Czy x jest liczbą pierwszą?");
+
+        if (X % 2 != 0 )
+        {
+            Console.WriteLine("Tak");
+
+
+        }
+        else
+        {
+            Console.WriteLine("Nie");
+        }
+        Console.WriteLine("Czy y jest liczbą pierwszą?");
+        if (y % 2 != 0 )
+        {
+            Console.WriteLine("Tak");
+            
+        }
+        else
+        {
+
+            Console.WriteLine("Nie");
+           
+        }
+        Console.ReadKey();
+    }
+
+
+
+    
+}
