19b3e669d32c0019311e2267164126329164383b
Marcel Klukiewicz <mklukiewicz@edu.cdv.pl>
2023-04-25 13:34:49 +0200

    ToDoWPF

---
+++ ToDoWPF/ToDo.core/ViewModels/Pages/WorkTaskPageViewModel.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using ToDo.core.ViewModels.Controllers;
+
+namespace ToDo.core.ViewModels.Pages
+{
+    public  class WorkTaskPageViewModel : BaseViewModel
+    {
+        public ObservableCollection<WorkTaskViewModel> WorkTaskList { get; set; } = new ObservableCollection<WorkTaskViewModel>();
+        public string  NewWorkTaskTitle { get; set; }
+
+        public string NewWorkTaskDescription { get;set; }
+
+        public ICommand AddNewTaskCommand { get; set; }// Icommmand fucnkcja odpowiedzialna za łączenie buttonów z metodami
+        public ICommand DeleteSelectedTaskCommand { get; set; }
+        public WorkTaskPageViewModel()
+        {
+            AddNewTaskCommand = new RelayCommand(AddnewTask);
+            DeleteSelectedTaskCommand = new RelayCommand(DeleteSelectedTask);
+        }
+
+
+        private void AddnewTask()
+        {
+            var newTask = new WorkTaskViewModel
+            {
+                Title = NewWorkTaskTitle,
+                Description = NewWorkTaskDescription,
+                CreatedDate = DateTime.Now
+             };
+
+            WorkTaskList.Add(newTask);
+
+            NewWorkTaskTitle = string.Empty; 
+            NewWorkTaskDescription = string.Empty;
+
+            OnPropertyChanged(nameof(NewWorkTaskTitle));
+            OnPropertyChanged(nameof(NewWorkTaskDescription));
+        }
+
+        private void DeleteSelectedTask() 
+        {
+            var selectedTasks = WorkTaskList.Where(x => x.IsSelected).ToList();
+
+            foreach(var task in selectedTasks)
+            {
+                WorkTaskList.Remove(task);
+            }
+        }
+    }
+}
---
+++ ToDoWPF/ToDoWPF/Pages/WorkTasksPage1.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System.Net.Http.Headers;
+using System.Windows.Controls;
+using ToDo.core.ViewModels.Controllers;
+using ToDo.core.ViewModels.Pages;
+
+namespace ToDoWPF
+{
+    /// <summary>
+    /// Logika interakcji dla klasy WorkTasksPage1.xaml
+    /// </summary>
+    public partial class WorkTasksPage1 : Page
+    {
+        public WorkTasksPage1()
+        {
+            InitializeComponent();
+
+            DataContext = new WorkTaskPageViewModel();
+        }
+    }
+}
